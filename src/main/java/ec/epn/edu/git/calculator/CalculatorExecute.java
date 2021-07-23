e4a90424e99ceec18c5d11144e9acf82d654ff6f
Christian Morán <christian.moran@epn.edu.ec>
2021-07-23 00:20:07 -0500

    Adding Calculator Execute Class and JAR. Morán-Pallo

--- src/main/java/ec/epn/edu/git/calculator/CalculatorExecute.java
+++ src/main/java/ec/epn/edu/git/calculator/CalculatorExecute.java
@@ -2,7 +2,7 @@ package ec.epn.edu.git.calculator;
 
 public class CalculatorExecute {
     public static void main(String[] args) {
-        System.out.println("== GRUPO 6 -Morán-Pallo- Calculador Execute ==");
+        System.out.println("== GRUPO 6 --Morán-Pallo-- Calculador Execute ==");
         Calculator c = new Calculator();
         int addition = c.addition(4, 7);
         System.out.println("c.addition(4,7) = " + addition);
